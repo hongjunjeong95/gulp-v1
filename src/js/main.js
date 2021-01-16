f32e825506b4b151fc65e479250d4c30dacfd9ce
zpskek <wjdghdwns0@gmail.com>
2021-01-16 17:25:13 +0900

    gulp-bro and babelify

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,1 @@
-import { random } from "./util";
-
-const rOne = random(10);
-const rTwo = random(20);
-
-console.log(`${rOne} ${rTwo}`);
+import "./utils";
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,1 +1,5 @@
-export const random = (max) => Math.floor(Math.random() * max);
+const random = (max) => Math.floor(Math.random() * max);
+const rOne = random(10);
+const rTwo = random(20);
+
+console.log(`${rOne} ${rTwo}`);
